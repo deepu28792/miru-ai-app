ce6345fcc0c74c45363fe0a44d8e5950425c39ca
deepu28792 <deepu28792@gmail.com>
2026-03-06 04:35:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MiruAI());
+}
+
+class MiruAI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Miru AI',
+      home: Scaffold(
+        appBar: AppBar(title: Text("Miru AI")),
+        body: Center(
+          child: ElevatedButton(
+            child: Text("Generate AI Video"),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
